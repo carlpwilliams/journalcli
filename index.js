7e37c2374d0cd0c01301004895d078eac68c1b1d
Carl Williams <51321972+carlWilliamsSage@users.noreply.github.com>
2019-06-05 08:02:03 +0100

    introduced inquirer, WIP

--- index.js
+++ index.js
@@ -1,34 +1,98 @@
 #!/usr/bin/env node
 // JavaScript source code
+require = require('esm')(module /*, options*/);
+
+
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const journal = require('./journal.js');
 
-console.info('running');
-clear();
+const Listr = require('listr');
 
-console.log(
-  chalk.yellow(
-    figlet.textSync('Carl Jour', { horizontalLayout: 'full' })
-  )
-);
+var inquirer = require('inquirer');
+
+
+let lastMessage = 'Running';
 
 const run = async () => {
-    console.info('run whatevs.');
-    //console.info(process);
-    //process.argv[2]
-    const [, , ...args] = process.argv;
-    switch (args[0]) {
-        case ".":
-            console.info(chalk.yellow('task added ') + args.join(" "));
-            break;
-        case "!":
-            console.info(chalk.yellow('fact added ') + args.join(" "));
-            break;
+  // clear();
+
+  console.log(
+    chalk.yellow(
+      figlet.textSync('Journal CLI', { horizontalLayout: 'full' })
+    )
+  );
+  console.log(
+    chalk.yellow(
+      lastMessage
+    )
+  );
+  
+  newPrompt().then(answer => {
+    if (answer === '') {
+      edit();
     }
-    //args.forEach(function (val, index, array) {
-    //    console.log(index + ': ' + val);
-    //});
+    else {
+      journal.addNewEntry(answer.entry.split(' '));
+    }
+    run();
+  });
+
+  // //console.info(process);
+  // //process.argv[2]
+  // const [, , ...args] = process.argv;
+  // switch (args[0]) {
+  //   case ".":
+  //     console.info(chalk.yellow('task added ') + args.join(" "));
+  //     break;
+  //   case "!":
+  //     console.info(chalk.yellow('fact added ') + args.join(" "));
+  //     break;
+  // }
+  // //args.forEach(function (val, index, array) {
+  // //    console.log(index + ': ' + val);
+  // //});
 };
+
+
+const newPrompt = async () => {
+  return new Promise((resolve, reject) => {
+    console.log(chalk.blue('Your Current Journal'));
+    journal.getJournal().then(res => {
+      console.info(res);
+      inquirer
+        .prompt([
+          {
+            type: 'entry',
+            name: 'entry',
+            choices:['a':'test'],
+            message: 'New Entry'
+          },
+        ])
+        .then(answers => {
+          // Use user feedback for... whatever!!
+          resolve(answers);
+        });
+    });
+  });
+}
+
+
+const edit = async () => {
+  const questions = [];
+  questions.push({
+    type: 'list',
+    name: 'template',
+    message: 'Please choose which project template to use',
+    choices: ['JavaScript', 'TypeScript'],
+  });
+
+
+  const answers = await inquirer.prompt(questions);
+
+}
+
+
 
 run();
